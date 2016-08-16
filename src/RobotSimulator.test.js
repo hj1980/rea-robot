7b07db1c10e94a51f8b9f35a46415b55d3e0a85c
Heath Jones <hj1980@gmail.com>
2016-08-16 11:27:30 +1000

    Add project testing and documentation dependencies

---
+++ src/RobotSimulator.test.js
@@ -0,0 +1,72 @@
+describe("RobotSimulator", function () {
+
+  // Verify instantiation
+  it("should not be able to be instantiated without a valid width and height", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should be able to be instantiated with a valid width and height", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should contain the correctly sized table surface in it's state.", function () {
+    throw new Error("not implemented yet");
+  });
+
+
+  // Verify command syntax.
+  it("should accept a syntactically correct PLACE command.", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should accept a syntactically correct MOVE command.", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should accept a syntactically correct RIGHT command.", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should accept a syntactically correct LEFT command.", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should accept a syntactically correct REPORT command.", function () {
+    throw new Error("not implemented yet");
+  });
+
+
+  // Verify command semantics.
+  it("should allow placement of Robot on the table.", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should allow subsequent placement of Robot on the table.", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should ignore placement of Robot off the table.", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should ignore all commands until a valid PLACE command.", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should accept a valid MOVE command.", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should not change state for an invalid MOVE command.", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should accept a valid command after an invalid MOVE command.", function () {
+    throw new Error("not implemented yet");
+  });
+
+  it("should silently ignore invalid commands.", function () {
+    throw new Error("not implemented yet");
+  });
+
+});
